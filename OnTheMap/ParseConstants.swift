533d563d1b7969b6444df104b6927849f6d98c1e
coolshubh4 <shubham.tripathi4@outlook.com>
2015-08-23 02:45:05 +0530

    Add Parse constants

--- OnTheMap/ParseConstants.swift
+++ OnTheMap/ParseConstants.swift
@@ -6,4 +6,41 @@
 //  Copyright (c) 2015 coolshubh4. All rights reserved.
 //
 
-extension ParseClient {}
+extension ParseClient {
+
+    struct constants {
+        
+        static let AppId = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let ApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        
+        static let BaseURL = "https://api.parse.com/1/"
+    }
+    
+    struct Methods {
+    
+        static let StudentLocation = "classes/StudentLocation"
+    }
+    
+    struct ParameterKeys {
+    
+        static let AppIdParam = "X-Parse-Application-Id"
+        static let ApiKeyParam = "X-Parse-REST-API-Key"
+    }
+    
+    
+    
+    struct JSONResponseKeys {
+    
+        static let ObjectId = "objectId"
+        static let UniqueKey = "uniqueKey"
+        static let FirstName = "firstName"
+        static let LastName = "lastName"
+        static let MapString = "mapString"
+        static let MediaURL = "mediaURL"
+        static let Latitude = "latitude"
+        static let Longitude = "longitude"
+        static let CreatedAt = "createdAt"
+        static let UpdatedAt = "updatedAt"
+        
+    }
+}
