5cf0035a518641410206ab19a98d794df5a28f67
coolshubh4 <shubham.tripathi4@outlook.com>
2015-08-23 02:17:05 +0530

    Add folder/files for MVC implementation

---
+++ OnTheMap/ParseConstants.swift
@@ -0,0 +1,9 @@
+//
+//  ParseConstants.swift
+//  OnTheMap
+//
+//  Created by Shubham Tripathi on 23/08/15.
+//  Copyright (c) 2015 coolshubh4. All rights reserved.
+//
+
+extension ParseClient {}
---
+++ OnTheMap/UdacityConstants.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityConstants.swift
+//  OnTheMap
+//
+//  Created by Shubham Tripathi on 23/08/15.
+//  Copyright (c) 2015 coolshubh4. All rights reserved.
+//
+
+extension UdacityClient {}
