a2963aefa66439fef11cd4892929f75ec2cdac46
coolshubh4 <shubham.tripathi4@outlook.com>
2015-08-23 02:57:44 +0530

    Add Udacity constants

--- OnTheMap/UdacityConstants.swift
+++ OnTheMap/UdacityConstants.swift
@@ -6,4 +6,24 @@
 //  Copyright (c) 2015 coolshubh4. All rights reserved.
 //
 
-extension UdacityClient {}
+extension UdacityClient {
+
+    struct constants {
+        
+        static let BaseURL = "https://www.udacity.com/api/"
+    }
+    
+    struct Methods {
+        
+        static let Session = "session"
+        static let User = "users/"
+    }
+    
+    struct JSONResponseKeys {
+        
+        static let SessionId = "id"
+        static let UserId = "key"
+        static let FirstName = "first_name"
+        static let LastName = "last_name"
+    }
+}
